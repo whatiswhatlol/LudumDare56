4625b6f841f4a1be8c4bd410ece8712e90c7c726
whatiswhatlol <94203006+whatiswhatlol@users.noreply.github.com>
2024-10-06 09:45:05 -0700

    scene done

---
+++ Assets/LeaderBoadManager.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using TMPro;
+
+// NOTE: Make sure to include the following namespace wherever you want to access Leaderboard Creator methods
+using 
+
+
+    public class LeaderboardManager : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text[] _entryTextObjects;
+        [SerializeField] private TMP_InputField _usernameInputField;
+
+        // Make changes to this section according to how you're storing the player's score:
+        // ------------------------------------------------------------
+        [SerializeField] private ExampleGame _exampleGame;
+
+        private int Score => _exampleGame.Score;
+        // ------------------------------------------------------------
+
+        private void Start()
+        {
+            LoadEntries();
+        }
+
+        private void LoadEntries()
+        {
+            // Q: How do I reference my own leaderboard?
+            // A: Leaderboards.<NameOfTheLeaderboard>
+
+            Leaderboards.TutorialLeaderboard.GetEntries(entries =>
+            {
+                foreach (var t in _entryTextObjects)
+                    t.text = "";
+
+                var length = Mathf.Min(_entryTextObjects.Length, entries.Length);
+                for (int i = 0; i < length; i++)
+                    _entryTextObjects[i].text = $"{entries[i].Rank}. {entries[i].Username} - {entries[i].Score}";
+            });
+        }
+
+        public void UploadEntry()
+        {
+            Leaderboards.TutorialLeaderboard.UploadNewEntry(_usernameInputField.text, Score, isSuccessful =>
+            {
+                if (isSuccessful)
+                    LoadEntries();
+            });
+        }
+    }
